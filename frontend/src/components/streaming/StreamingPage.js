f491b7afda8c3263e8f7d1e5e3cffc46e5956767
david-at-109308348564 <se.davidsoria@gmail.com>
2025-01-28 13:10:46 +0100

    WebSocket Client en Streaming Page y nuevo Reproductor HLS por conflito con web sockets

--- frontend/src/components/streaming/StreamingPage.js
+++ frontend/src/components/streaming/StreamingPage.js
@@ -2,7 +2,7 @@ import { Box } from "@mui/material";
 import LogsTimeline from "./LogsTimeline";
 import Stream from "./Stream";
 import { useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useGetHlsStreamUrlQuery, useGetStreamsQuery } from "../../api/streamsApi";
 import StreamSkeleton from "../skeletons/StreamSkeleton";
 
@@ -10,14 +10,56 @@ export default function StreamingPage() {
   const location = useLocation();
   const { streamName } = location.state || {}; // Recupera el nombre desde el state
 
-  const { data: streamsData } = useGetStreamsQuery();
+  const { data: streamsData, isLoading: isStreamsLoading } = useGetStreamsQuery();
 
   const urlOffline = streamsData?.find((stream) => stream.stream_name === streamName)?.url;
-  const { data: hlsUrl, error, isLoading } = useGetHlsStreamUrlQuery(streamName, {
-    skip: urlOffline, // Usa `skip` si `urlOffline` existe
+  
+  // Solo realiza la segunda llamada cuando se haya completado la primera y no se haya encontrado `urlOffline`.
+  const skipSecondCall = isStreamsLoading || Boolean(urlOffline);
+  
+  const { data: hlsUrl, error, isLoading: isHlsLoading } = useGetHlsStreamUrlQuery(streamName, {
+    skip: skipSecondCall, // Usa `skip` si la primera llamada está cargando o ya existe `urlOffline`.
   });
 
+  const isLoading = isStreamsLoading || isHlsLoading;
+
+  const wsRef = useRef(null); // Referencia al WebSocket para asegurarnos de que no se recree
+  
   const [currentUrl, setCurrentUrl] = useState(null);
+
+
+  // Configurar el WebSocket
+  useEffect(() => {
+    // wsRef.current = new WebSocket("wss://ws.postman-echo.com/raw");
+    console.log("Creando web-socket cliente")
+    wsRef.current = new WebSocket("ws://localhost:8765");
+    const ws = wsRef.current;
+
+    ws.onopen = () => {
+      console.log("WebSocket connected");
+    };
+
+    ws.onmessage = (event) => {
+      console.log(event.data)
+      const data = JSON.parse(event.data); // Datos JSON recibidos
+      if (Array.isArray(data)) {
+        console.log(data); // Actualizar bounding boxes
+      }
+    };
+
+    ws.onerror = (error) => {
+      console.error("WebSocket error:", error);
+    };
+
+    ws.onclose = () => {
+      console.log("WebSocket disconnected");
+    };
+
+    return () => {
+      ws.close(); // Cerrar el WebSocket cuando se desmonte
+    };
+  }, []);
+
 
   useEffect(() => {
     if (hlsUrl) {
@@ -26,6 +68,7 @@ export default function StreamingPage() {
     else if (urlOffline)
       setCurrentUrl(urlOffline); // Usa la URL offline en caso de error
   }, [hlsUrl, urlOffline]);
+
 
   if (error && error.status === 404) {
     const errorMessage = error.data?.message || "Error desconocido";
